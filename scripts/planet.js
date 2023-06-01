e75dde1139b14c0faf100c96ba87b45131b80a75
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-01 15:59:37 +0800

    Add files via upload

---
+++ scripts/planet.js
@@ -0,0 +1,12 @@
+// Change the planet generator
+// Using ErekirPlanetGenerator prevents people to generate random sectors
+Events.on(ContentInitEvent, e => {
+    Vars.content.planet("serpul_clones-serpulq").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-serpulr").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-serpuls").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-serpult").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-serpulu").generator = altered_spg_alt();
+
+});
+
+function altered_spg_alt(){var a = new SerpuloPlanetGenerator(); a.alt = true; return a;}
