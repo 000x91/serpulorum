a8e79b17a649e3e9acdf90361e42f250baab7be6
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 15:13:53 +0800

    Update planet.js

--- scripts/planet.js
+++ scripts/planet.js
@@ -6,9 +6,9 @@ Events.on(ContentInitEvent, e => {
     Vars.content.planet("serpul_clones-serpuls").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpult").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulu").generator = altered_spg_alt();
-    Vars.content.planet("serpul_clones-serpulv").generator = empty_extension; 
+    Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low;  
 
 });
 
 function altered_spg_alt(){var a = new SerpuloPlanetGenerator(); a.alt = true; return a;}
-var empty_extension = extend(SerpuloPlanetGenerator,{allowLanding(s){return true}});
+var unlock_all_sectors_all_low = extend(SerpuloPlanetGenerator,{allowLanding(s){return true}}); //unlock all sectors and turn all low. 
