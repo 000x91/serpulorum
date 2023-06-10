9901c3b2ad5064d202bdf9928a4e33e4c0608e56
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 20:40:39 +0800

    Update planet.js

--- scripts/planet.js
+++ scripts/planet.js
@@ -1,13 +1,13 @@
 // Change the planet generator
 // Using ErekirPlanetGenerator prevents people to generate random sectors
 Events.on(ContentInitEvent, e => {
+    Vars.content.planet("setpul_clones-s1_genlake_false").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulq").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulr").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpuls").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpult").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulu").generator = altered_spg_alt();
     Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low; // that does not work. 
-
 });
 
 function altered_spg_alt(){var a = new SerpuloPlanetGenerator(); a.alt = true; return a;}
