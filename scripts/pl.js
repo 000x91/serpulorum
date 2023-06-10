b718cef3a7fdeab9c9003882420991dc610fd866
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 21:14:20 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -8,7 +8,7 @@ Events.on(ContentInitEvent, e => {
     Vars.content.planet("serpul_clones-serpuls").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpult").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulu").generator = altered_spg_alt();
-    Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low; // that does not work. 
+    Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low; //that works. 
 });
 
 function altered_spg_alt(){var a = new SerpuloPlanetGenerator(); a.alt = true; return a;}
