6d2de9d353aac9e30150457b804cbf00e52c7480
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 15:40:58 +0800

    Update planet.js
    added some genlakes

--- scripts/planet.js
+++ scripts/planet.js
@@ -6,9 +6,11 @@ Events.on(ContentInitEvent, e => {
     Vars.content.planet("serpul_clones-serpuls").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpult").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulu").generator = altered_spg_alt();
-    Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low;  
+    Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low; //all sectors in this planet are unlocked. 
 
 });
 
 function altered_spg_alt(){var a = new SerpuloPlanetGenerator(); a.alt = true; return a;}
-var unlock_all_sectors_all_low = extend(SerpuloPlanetGenerator,{allowLanding(s){return true}}); //unlock all sectors and turn all low. 
+function altered_spg_waterOffset(a){var b = extend(SerpuloPlanetGenerator,{waterOffset = a;}); return b;}
+var genLakes_true = extend(SerpuloPlanetGenerator,{genLakes = true});
+var unlock_all_sectors_all_low = extend(SerpuloPlanetGenerator,{allowLanding(s){return true}}); //unlock all sectors and turn all low.
