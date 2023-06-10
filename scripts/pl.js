f27d9a4ab190c41774af5300f8fac79c3ce7b572
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 21:08:22 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -1,16 +1,16 @@
 // Change the planet generator
 // Using ErekirPlanetGenerator prevents people to generate random sectors
 Events.on(ContentInitEvent, e => {
-    Vars.content.planet("serpul_clones-s1_genlakes_false").generator = new SerpuloPlanetGenerator();
-    Vars.content.planet("serpul_clones-s1_genlakes_true").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-s1_r5.67").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-s1_r9.99").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulq").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulr").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpuls").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpult").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulu").generator = altered_spg_alt();
-    Vars.content.planet("serpul_clones-serpulv").generator = new SerpuloPlanetGenerator();; // that does not work. 
+    Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low; // that does not work. 
 });
 
 function altered_spg_alt(){var a = new SerpuloPlanetGenerator(); a.alt = true; return a;}
-var genLakes_true = extend(SerpuloPlanetGenerator,{genLakes = true});
-//var unlock_all_sectors_all_low = extend(SerpuloPlanetGenerator,{allowLanding(s){return true}}); //unlock all sectors and turn all low.
+//var genLakes_true = extend(SerpuloPlanetGenerator,{genLakes = true}); // causes js to stop working altogether. 
+var unlock_all_sectors_all_low = extend(SerpuloPlanetGenerator,{allowLanding(s){return true}}); //unlock all sectors and turn all low.
