3a8250f0313432d0c8e3e808571c87fa603c8f03
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-10 20:59:07 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -2,13 +2,13 @@
 // Using ErekirPlanetGenerator prevents people to generate random sectors
 Events.on(ContentInitEvent, e => {
     Vars.content.planet("serpul_clones-s1_genlakes_false").generator = new SerpuloPlanetGenerator();
-    Vars.content.planet("serpul_clones-s1_genlakes_true").generator = genLakes_true;
+    Vars.content.planet("serpul_clones-s1_genlakes_true").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulq").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpulr").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpuls").generator = new SerpuloPlanetGenerator();
     Vars.content.planet("serpul_clones-serpult").generator = new SerpuloPlanetGenerator();
-    Vars.content.planet("serpul_clones-serpulu").generator = altered_spg_alt();
-    Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low; // that does not work. 
+    Vars.content.planet("serpul_clones-serpulu").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-serpulv").generator = new SerpuloPlanetGenerator();; // that does not work. 
 });
 
 function altered_spg_alt(){var a = new SerpuloPlanetGenerator(); a.alt = true; return a;}
