27d1e976a5020a1fcd23f18fcda70f597d364829
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-11 23:11:31 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -13,6 +13,6 @@ Events.on(ContentInitEvent, e => {
 });
 
 function altered_spg_alt(){var a = new SerpuloPlanetGenerator(); a.alt = true; return a;}
-//var genLakes_true = extend(SerpuloPlanetGenerator,{genLakes = true}); // causes js to stop working altogether. 
 var all_enemy_bases = extend(SerpuloPlanetGenerator,{generateSector(sector){sector.generateEnemyBase=true}});
+var genLakes_true = extend(SerpuloPlanetGenerator,{{genLakes = true}}); // causes js to stop working altogether. 
 var unlock_all_sectors_all_low = extend(SerpuloPlanetGenerator,{allowLanding(s){return true}}); //unlock all sectors and turn all low.
