b1124da0c7aff61154323e67ba0331a4c6fb0df1
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-13 14:42:28 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -2,12 +2,6 @@
 // Using ErekirPlanetGenerator prevents people to generate random sectors
 Events.on(ContentInitEvent, e => {
     Vars.content.planet("serpul_clones-crux_lair").generator = all_enemy_bases; //currently does not work. 
-    Vars.content.planet("serpul_clones-s1_r5.67").generator = new SerpuloPlanetGenerator();
-    Vars.content.planet("serpul_clones-s1_r9.99").generator = new SerpuloPlanetGenerator(); //same seed different sizes will make the maps different
-    Vars.content.planet("serpul_clones-serpulq").generator = new SerpuloPlanetGenerator();
-    Vars.content.planet("serpul_clones-serpulr").generator = new SerpuloPlanetGenerator();
-    Vars.content.planet("serpul_clones-serpuls").generator = new SerpuloPlanetGenerator();
-    Vars.content.planet("serpul_clones-serpult").generator = new SerpuloPlanetGenerator(); // the larger the planet size, the larger map size 
     Vars.content.planet("serpul_clones-serpulu").generator = altered_spg_alt();
     Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low; //that works. 
 });
