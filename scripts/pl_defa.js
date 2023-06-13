b5129f7c03cb74e92f9629ea59b771318325694a
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-06-13 14:42:48 +0800

    Update pl_defa.js

--- scripts/pl_defa.js
+++ scripts/pl_defa.js
@@ -1,1 +1,11 @@
+Events.on(ContentInitEvent, e => {
+
+    Vars.content.planet("serpul_clones-s1_r5.67").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-s1_r9.99").generator = new SerpuloPlanetGenerator(); //same seed different sizes will make the maps different
+    Vars.content.planet("serpul_clones-serpulq").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-serpulr").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-serpuls").generator = new SerpuloPlanetGenerator();
+    Vars.content.planet("serpul_clones-serpult").generator = new SerpuloPlanetGenerator(); // the larger the planet size, the larger map size 
+
+});
 
