0929f3e3a9f6142b19bbdae1335838f9e607dc6a
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2023-08-11 12:47:51 +0800

    Update pl.js

--- scripts/pl.js
+++ scripts/pl.js
@@ -5,7 +5,7 @@
 Events.on(ContentInitEvent, e => {
     Vars.content.planet("serpul_clones-core_zones").generator = alt_genTile; 
     Vars.content.planet("serpul_clones-crux_lair").generator = all_enemy_bases;//currently does not work. 
-    Vars.content.planet("serpul_clones-highdiff").generator = hidiff; 
+    Vars.content.planet("serpul_clones-highdiff").generator = all_enemy_bases; 
     Vars.content.planet("serpul_clones-serpulu").generator = altered_spg_alt();//altered_spg_alt();
     Vars.content.planet("serpul_clones-serpulv").generator = unlock_all_sectors_all_low; //maybe changed later. 
 });
